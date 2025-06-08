6bfb40b3d2a41de1f21964649673931804571a81
SaidRuziev <saidruziev24@gmail.com>
2025-06-08 20:49:17 +0600

    Add TempConverter

---
+++ ProjectOne/DLL.TempConverter/TempConverter.cs
@@ -0,0 +1,11 @@
+﻿namespace DLL.TempConverter;
+
+public class TempConverter
+{
+    /// <summary>
+    ///     
+    /// </summary>
+    /// <param name="temp"></param>
+    /// <returns></returns>
+    public double ConvertToKelvin(double temp) => temp + 273;
+}
